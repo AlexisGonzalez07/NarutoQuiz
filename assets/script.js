564a4c1a089cffff72773c30d5cd472f18ed7ae9
AlexisGonzalez07 <alexis.gonzalez07@gmail.com>
2021-09-28 17:10:22 -0700

    Developed iteration of question and answer display, added answer check function, needs scoring and end of Game function

--- assets/script.js
+++ assets/script.js
@@ -1,88 +1,112 @@
-var button = $("<button>")
-
-var questionArray = [
-    "How was Minato Namikaze related to Naruto?",
-    "Who was Naruto's best friend and greatest foe?",
-    "What was the nickname that Naruto gaver to Jiraiya Sensei?",
-    "Who was the creater of the Reanimation Jutsu?",
-    "What is Naruto's ninja way?"
-]
-
-var question1Answers = [
-    "He was his father",
-    "He was the Fourth Hokage",
-    "He was Naruto's sensei",
-    "He was Naruto's brother"
-]
-
-var question2Answers = [
-    "Madara Uchiha",
-    "Sasuke Uchiha",
-    "Orochimaru",
-    "Sakura"
-]
-
-var question3Answers = [
-    "Master Sensei",
-    "The Great Sage",
-    "Legendary Sannin",
-    "Pervy Sage"
-]
-
-var question4Answers = [
-    "Orochimaru",
-    "Kabuto",
-    "Sasori, the Puppet Master",
-    "Lady Tsunade"
-]
-
-var question5Answers = [
-    "A true Shinobi will die for a comrade",
-    "I never give up",
-    "I never go back on my word",
-    "I'll do anything to protect the Hidden Leaf"
-]
-
-// Check for errors in syntax on variable creation
-// console.log(questionArray)
-// console.log(question1Answers)
-// console.log(question2Answers)
-// console.log(question3Answers)
-// console.log(question4Answers)
-// console.log(question5Answers)
-
-// Gather all buttons
-var startButton = $('.btn')
-// Set listening event for start Game
-startButton.on('click', startGame)
-// Start game function
-function startGame(_event){
-    timerCountdown();
-
-
-
-var secondsLeft = 10
+var question = $("#question")
+var currentQuestion = 0
+var secondsLeft = 60
 var timeEl = $('#timer')
+var grid = $('#answer-grid')
+var body = $('body')
+var questionArray = [
+    {
+        question:"How was Minato Namikaze related to Naruto?",
+        answer: "He was his father",
+        options:  ["He was his father","He was the Fourth Hokage","He was Naruto's sensei","He was Naruto's brother"]
+    },
+    {
+        question:"Who was Naruto's best friend and greatest foe?",
+        answer: "Sasuke Uchiha",
+        options:  ["Madara Uchiha", "Sasuke Uchiha", "Orochimaru", "Sakura"]
+    },
+    {
+        question:"What was the nickname that Naruto gaver to Jiraiya Sensei?",
+        answer: "Pervy Sage",
+        options:  ["Pervy Sage", "Master Sensei", "Orochimaru", "Sakura"]
+    },
+    {
+        question:"Who was the creater of the Reanimation Jutsu?",
+        answer: "Orochimaru",
+        options:  ["Orochimaru", "Kabuto", "Sasori, the Puppet Master", "Lady Tsunade"]
+    },
+    {
+        question:"What is Naruto's ninja way?",
+        answer: "I never go back on my word",
+        options:  ["A true Shinobi will die for a comrade",  "I never give up", "I'll do anything to protect the Hidden Leaf", "I never go back on my word"]
+    }]
 
-function timerCountdown() {
-    var timerInterval = setInterval(function(){
-    timeEl.text(secondsLeft + ' seconds left');
-    secondsLeft--;
+    var startButton = $('#bottom-btn')
+    // Set listening event for start Game
+    // Start game function
+    function startGame(event){
+        startButton.text('');
+        startButton.text('Next');  
+        timerCountdown();
+        updateQuiz();
+    
+    
+    
+    
+    function timerCountdown() {
+        var timerInterval = setInterval(function(){
+        timeEl.textContent = secondsLeft + " seconds left";
+        secondsLeft--;
+    
+        if(secondsLeft === 0) {
+            alert('Game Over. Restarting');
+            clearInterval(timerInterval);
+            timeEl.text('')}
+    
+    }, 1000)}
 
-    if(secondsLeft === 0) {
-        alert('Game Over. Restarting');
-        console.log("end");
-        clearInterval(timerInterval);
-        timeEl.text('')
+    function updateQuiz() {
+        body.removeClass();
+        grid.empty()
+        question.text('')
+        question.text(questionArray[currentQuestion].question)};
+        console.log(questionArray[currentQuestion].options.length)
+        for (var i = 0; i < questionArray[currentQuestion].options.length; i++) { 
+        let btn = document.createElement('button');
+        btn.textContent = questionArray[currentQuestion].options[i]
+        btn.value=questionArray[currentQuestion].options[i]
+        btn.classList.add("btn")
+        btn.addEventListener('click', function(){
+            checkAnswer(this.value);
+            currentQuestion++;
+            })
+        grid.append(btn)
+    }
+
+   
+    }
+
+    
+    function checkAnswer(selectedAnswer){
+        //var selectedAnswer = this.value
+        
+        if (currentQuestion === questionArray.length) {
+            alert('Game Over. Restarting');
+            clearInterval(timerInterval);
+            timeEl.text('');
+            grid.empty()
+        } 
+        console.log(selectedAnswer,questionArray[currentQuestion].answer)
+        if (selectedAnswer === questionArray[currentQuestion].answer) {
+            // score variable +
+            body.addClass("correct")
+        } else {
+            body.addClass("wrong")
+
         }
 
-}, 1000)}
-}
 
-// function setNextQuestion() {
+    }
+    startButton.on('click', startGame)
+    
+    
+    
+    // check answer
 
-// }
+    // check if last question, if not, next question
 
-// function endGame() {
+    // end game function with local storage
 
-// }
+
+// 21,23,25
+
